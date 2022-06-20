d25062809dc92dcd65acf09c1dc45557d9bc90ba
regina <gidirimr@gmail.com>
2022-06-20 17:43:13 +0300

    hw1

---
+++ OnlineStoreApplication/OnlineStoreApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlineStoreApplication
+//
+//  Created by Macbook on 20.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+
+
+}
+
